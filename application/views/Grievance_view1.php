9dd1ed9506b20f9508a57cf286031fa6595c9963
name-hacke <tamiltutorchannel@gmail.com>
2019-01-31 10:58:52 +0530

    finished validation

--- application/views/Grievance_view1.php
+++ application/views/Grievance_view1.php
@@ -3,6 +3,7 @@
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
+    <script src="//cdn.ckeditor.com/4.11.2/standard/ckeditor.js"></script>
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
@@ -28,6 +29,15 @@
           </form>
         </div>
       </div>
+      <div class="row">
+        <div class="col-lg-10">
+          <textarea name="editor1"></textarea>
+
+        </div>
+      </div>
     </div>
+    <script>
+            CKEDITOR.replace( 'editor1' );
+    </script>
   </body>
 </html>
